54ccbae2563beb6278896cac1eb780c66cc55cef
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2021-05-20 17:29:21 +0200

    [code coverage] follow main pipeline setup

--- vars/kibanaCoverage.groovy
+++ vars/kibanaCoverage.groovy
@@ -215,39 +215,81 @@ def runTests() {
   ])
 }
 
-def ossProks() {
-  return [
-    'oss-ciGroup1' : kibanaPipeline.ossCiGroupProcess(1),
-    'oss-ciGroup2' : kibanaPipeline.ossCiGroupProcess(2),
-    'oss-ciGroup3' : kibanaPipeline.ossCiGroupProcess(3),
-    'oss-ciGroup4' : kibanaPipeline.ossCiGroupProcess(4),
-    'oss-ciGroup5' : kibanaPipeline.ossCiGroupProcess(5),
-    'oss-ciGroup6' : kibanaPipeline.ossCiGroupProcess(6),
-    'oss-ciGroup7' : kibanaPipeline.ossCiGroupProcess(7),
-    'oss-ciGroup8' : kibanaPipeline.ossCiGroupProcess(8),
-    'oss-ciGroup9' : kibanaPipeline.ossCiGroupProcess(9),
-    'oss-ciGroup10': kibanaPipeline.ossCiGroupProcess(10),
-    'oss-ciGroup11': kibanaPipeline.ossCiGroupProcess(11),
-    'oss-ciGroup12': kibanaPipeline.ossCiGroupProcess(12),
-  ]
+def setKibanaEnv(maxWorkers = '') {
+  notifyOnError {
+    withEnv(["KBN_OPTIMIZER_MAX_WORKERS=${maxWorkers}"]) {
+      runbld("./test/scripts/jenkins_code_coverage.sh", "Verify tests")
+    }
 }
 
-def xpackProks() {
-  return [
-    'xpack-ciGroup1' : kibanaPipeline.xpackCiGroupProcess(1),
-    'xpack-ciGroup2' : kibanaPipeline.xpackCiGroupProcess(2),
-    'xpack-ciGroup3' : kibanaPipeline.xpackCiGroupProcess(3),
-    'xpack-ciGroup4' : kibanaPipeline.xpackCiGroupProcess(4),
-    'xpack-ciGroup5' : kibanaPipeline.xpackCiGroupProcess(5),
-    'xpack-ciGroup6' : kibanaPipeline.xpackCiGroupProcess(6),
-    'xpack-ciGroup7' : kibanaPipeline.xpackCiGroupProcess(7),
-    'xpack-ciGroup8' : kibanaPipeline.xpackCiGroupProcess(8),
-    'xpack-ciGroup9' : kibanaPipeline.xpackCiGroupProcess(9),
-    'xpack-ciGroup10': kibanaPipeline.xpackCiGroupProcess(10),
-    'xpack-ciGroup11': kibanaPipeline.xpackCiGroupProcess(11),
-    'xpack-ciGroup12': kibanaPipeline.xpackCiGroupProcess(12),
-    'xpack-ciGroup13': kibanaPipeline.xpackCiGroupProcess(13),
-  ]
+def allCoverageTasks() {
+  parallel([
+    general: {
+      withTasks {
+        task {
+          setKibanaEnv(16)
+          ossCiGroups()
+          xpackCiGroups()
+        }
+      }
+    },
+    jest: {
+      workers.ci(name: 'jest', size: 'n2-standard-16', ramDisk: false) {
+        catchErrors {
+          kibanaPipeline.scriptTask('Jest Unit Tests', 'test/scripts/test/jest_unit.sh')()
+        }
+
+        catchErrors {
+          runbld.junit()
+        }
+      }
+    },
+  ])
 }
+
+def ossCiGroups() {
+  def ciGroups = 1..12
+  tasks(ciGroups.collect { kibanaPipeline.ossCiGroupProcess(it, true) })
+}
+
+def xpackCiGroups() {
+  def ciGroups = 1..13
+  tasks(ciGroups.collect { kibanaPipeline.xpackCiGroupProcess(it, true) })
+}
+
+// def ossProks() {
+//   return [
+//     'oss-ciGroup1' : kibanaPipeline.ossCiGroupProcess(1),
+//     'oss-ciGroup2' : kibanaPipeline.ossCiGroupProcess(2),
+//     'oss-ciGroup3' : kibanaPipeline.ossCiGroupProcess(3),
+//     'oss-ciGroup4' : kibanaPipeline.ossCiGroupProcess(4),
+//     'oss-ciGroup5' : kibanaPipeline.ossCiGroupProcess(5),
+//     'oss-ciGroup6' : kibanaPipeline.ossCiGroupProcess(6),
+//     'oss-ciGroup7' : kibanaPipeline.ossCiGroupProcess(7),
+//     'oss-ciGroup8' : kibanaPipeline.ossCiGroupProcess(8),
+//     'oss-ciGroup9' : kibanaPipeline.ossCiGroupProcess(9),
+//     'oss-ciGroup10': kibanaPipeline.ossCiGroupProcess(10),
+//     'oss-ciGroup11': kibanaPipeline.ossCiGroupProcess(11),
+//     'oss-ciGroup12': kibanaPipeline.ossCiGroupProcess(12),
+//   ]
+// }
+
+// def xpackProks() {
+//   return [
+//     'xpack-ciGroup1' : kibanaPipeline.xpackCiGroupProcess(1),
+//     'xpack-ciGroup2' : kibanaPipeline.xpackCiGroupProcess(2),
+//     'xpack-ciGroup3' : kibanaPipeline.xpackCiGroupProcess(3),
+//     'xpack-ciGroup4' : kibanaPipeline.xpackCiGroupProcess(4),
+//     'xpack-ciGroup5' : kibanaPipeline.xpackCiGroupProcess(5),
+//     'xpack-ciGroup6' : kibanaPipeline.xpackCiGroupProcess(6),
+//     'xpack-ciGroup7' : kibanaPipeline.xpackCiGroupProcess(7),
+//     'xpack-ciGroup8' : kibanaPipeline.xpackCiGroupProcess(8),
+//     'xpack-ciGroup9' : kibanaPipeline.xpackCiGroupProcess(9),
+//     'xpack-ciGroup10': kibanaPipeline.xpackCiGroupProcess(10),
+//     'xpack-ciGroup11': kibanaPipeline.xpackCiGroupProcess(11),
+//     'xpack-ciGroup12': kibanaPipeline.xpackCiGroupProcess(12),
+//     'xpack-ciGroup13': kibanaPipeline.xpackCiGroupProcess(13),
+//   ]
+// }
 
 return this
