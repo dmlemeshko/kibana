b1663bd196d90e5b0eea4984901d5433d90b3045
Dzmitry Lemechko <dzmitry.lemechko@elastic.co>
2022-08-17 19:20:25 +0200

    fix

--- .buildkite/scripts/steps/scalability/benchmarking.sh
+++ .buildkite/scripts/steps/scalability/benchmarking.sh
@@ -18,23 +18,23 @@ ls -la
 echo "--- downloading latest hash"
 
 GCS_BUCKET="gs://kibana-performance/scalability-tests"
-# mkdir gcs_artefacts
-gsutil cp "$GCS_BUCKET/LATEST" "$WORKSPACE"
-HASH=`cat LATEST`
+mkdir gcs_artefacts
+gsutil cp "$GCS_BUCKET/LATEST" gcs_artefacts/
+HASH=`cat gcs_artefacts/LATEST`
 
 echo "--- downloading latest artefacts from single user performance run"
-gsutil cp -r "$GCS_BUCKET/$HASH" "$WORKSPACE"
+gsutil cp -r "$GCS_BUCKET/$HASH" gcs_artefacts/
 
 ls -la
 
 echo "--- creating $KIBANA_BUILD_LOCATION & unziping kibana build"
 
 mkdir -p "$KIBANA_BUILD_LOCATION"
-tar -xzf kibana-default.tar.gz -C "$KIBANA_BUILD_LOCATION" --strip=1
+tar -xzf gcs_artefacts/kibana-default.tar.gz -C "$KIBANA_BUILD_LOCATION" --strip=1
 
 cd "$KIBANA_DIR"
 
-tar -xzf ../kibana-default-plugins.tar.gz
+tar -xzf ../gcs_artefacts/kibana-default-plugins.tar.gz
 
 # unset env vars defined in other parts of CI for automatic APM collection of
 # Kibana. We manage APM config in our FTR config and performance service, and
@@ -65,9 +65,9 @@ echo "--- Cloning kibana-load-testing repo and preparing workspace"
 # KIBANA_LOAD_TESTING_GIT_COMMIT="$(git rev-parse HEAD)"
 # export KIBANA_LOAD_TESTING_GIT_COMMIT
 
-#export SCALABILITY_JOURNEYS_ROOT_PATH=/Users/dmle/github/kibana-load-testing
+tar -xzf ../gcs_artefacts/scalability_traces.tar.gz
 
-tar -xzf ../scalability_traces.tar.gz
+export SCALABILITY_JOURNEYS_ROOT_PATH="$KIBANA_DIR/scalability_traces/server"
 
 node scripts/functional_tests \
   --config x-pack/test/performance/scalability/config.ts \
